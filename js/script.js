7997c59f9e658e5d8dbe7d9c6752b6fb7a4c97f5
Unknown <thomasdidier@outlook.fr>
2018-03-15 16:50:58 +0100

    animation js fini

--- js/script.js
+++ js/script.js
@@ -72,3 +72,110 @@ $(document).ready(function(){
 
 
 });
+
+
+/* active menu item on clic*/
+$(document).ready(function(){
+
+    'use strict';
+
+    $('.navbar-nav li a').click(function(){
+
+        'use strict';
+
+        $('.navbar-nav li a').parent().removeClass("active");
+
+        $(this).parent().addClass("active");
+
+    });
+
+});
+
+
+//highlight menu item on scroll
+
+$(document).ready(function(){
+
+    'use strict';
+
+    $(window).scroll(function(){
+
+        'use strict';
+
+        $("section").each(function(){
+
+            'use strict';
+
+            var bb = $(this).attr("id");
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+
+            if($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei){
+
+                $(".navbar-nav li a[href = '#" + bb + "']").parent().addClass("active");
+
+            } else{
+
+                $(".navbar-nav li a[href = '#" + bb + "']").parent().removeClass("active");
+            }
+
+        });
+
+    });
+});
+
+//add auto padding to header
+$(document).ready(function(){
+
+	'use strict';
+
+	setInterval(function(){
+
+		'use strict';
+
+		var windowHeight = $(window).height();
+
+		var containerHeight = $(".header-container").height();
+
+		var padTop = windowHeight -containerHeight;
+
+		$(".header-container").css ({
+			'padding-top': Math.round(padTop / 2) + 'px',
+			'padding-bottom': Math.round(padTop / 2) + 'px',
+
+		});
+
+	}, 10)
+
+});
+
+//add bx slider
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+
+      slideWidth: 292.5,
+      auto: true,
+      minSlides: 1,
+      maxSlides: 3,
+      slideMargin: 150
+  });
+});
+
+// add counter
+
+$(document).ready(function(){
+
+	$('.counter-num').counterUp({
+    	delay: 10,
+    	time: 2000
+	});
+});
+
+// add animation/ initialize wow
+
+$(document).ready(function(){
+	'use strict'
+
+	new WOW().init();
+});
